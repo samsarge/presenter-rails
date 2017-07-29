ffdeb56b0657f727dcdb006de92e0e0b570ffca1
Sam Sargent <samsarge@hotmail.co.uk>
2017-07-29 19:47:19 +0100

    methods for correctly dynamically delegating methods to instance variables of passed in models based on naming conventions

--- lib/presenter/base.rb
+++ lib/presenter/base.rb
@@ -1,4 +1,17 @@
 module Presenter
   class Base
+    protected
+    def method_missing(method, *args, &block)
+      model_instance.send(method, *args, &block)
+    end
+
+    def respond_to_missing?(method, include_all = false)
+      model_instance.respond_to?(method, include_all)
+    end
+
+    def model_instance
+      model_name = self.class.to_s.underscore.sub(/_presenter/, '')
+      instance_variable_get(model_name.prepend('@'))
+    end
   end
 end
