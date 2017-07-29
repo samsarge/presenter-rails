1ca2dbf5e76e18d93c820446e1095a1197225525
Sam Sargent <samsarge@hotmail.co.uk>
2017-07-29 19:44:37 +0100

    text #method_missing and #respond_to? methods trusting that they delegate methods correctly to instance variable

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 describe Presenter::Base do
-  it 'should be true' do
-    expect(Presenter::Base).to be_truthy
+  context 'all presenter models' do
+    User = Struct.new(:id, :first_name, :last_name)
+    let(:user) { User.new(1, "Sam", "Sargent")}
+    let(:presenter) { UserPresenter.new(user) }
+
+    describe '#method_missing' do
+      it 'should call the method from the model passed in if it does not exist in the presenter' do
+        expect(presenter.class.ancestors).to include Presenter::Base
+        expect(presenter.first_name).to eq user.first_name
+      end
+    end
+
+    describe '#respond_to?' do
+      it 'should delegate #respond_to? to the instance variable of the model if the presenter does not respond to it' do
+        expect(presenter).to respond_to(:id)
+        # Normally the presenter would not RESPOND TO id, as technically id is
+        # not a method on the presenter (due to it getting delegated to the instance variable)
+      end
+    end
   end
 end
