bdf53f4c4d6b3800474d4a460e6668ddd6e87991
finn-francis <finn_francis@yahoo.com>
2017-07-30 13:18:54 +0100

    Added model instance getter for Presenter::Base

--- lib/presenter/base.rb
+++ lib/presenter/base.rb
@@ -1,17 +1,27 @@
 module Presenter
   class Base
+    def initialize(model)
+      @model = model
+      generate_model_instance_getter
+    end
+
     protected
+    def generate_model_instance_getter
+      define_singleton_method model_name do
+        @model
+      end
+    end
+
     def method_missing(method, *args, &block)
-      model_instance.send(method, *args, &block)
+      @model.send(method, *args, &block)
     end
 
     def respond_to_missing?(method, include_all = false)
-      model_instance.respond_to?(method, include_all)
+      @model.respond_to?(method, include_all)
     end
 
-    def model_instance
-      model_name = self.class.to_s.underscore.sub(/_presenter/, '')
-      instance_variable_get(model_name.prepend('@'))
+    def model_name
+      self.class.to_s.underscore.sub('_presenter', '')
     end
   end
 end
