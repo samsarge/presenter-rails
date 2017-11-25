2109a491e342a65298e5bb223b4e794fcef06e1f
finn-francis <finn_francis@yahoo.com>
2017-11-25 12:13:23 +0000

    Replace the getter method with #subject

--- lib/presenter/base.rb
+++ lib/presenter/base.rb
@@ -2,24 +2,20 @@ module Presenter
   class Base
     include Presenter::Naming
 
-    def initialize(model)
-      @model = model
-      generate_model_instance_getter
+    attr_reader :subject
+
+    def initialize(subject)
+      @subject = subject
     end
 
     protected
-    def generate_model_instance_getter
-      define_singleton_method model_object_name_from_presenter(@model) do
-        @model
-      end
-    end
 
     def method_missing(method, *args, &block)
-      @model.send(method, *args, &block)
+      subject.send(method, *args, &block)
     end
 
     def respond_to_missing?(method, include_all = false)
-      @model.respond_to?(method, include_all)
+      subject.respond_to?(method, include_all)
     end
   end
 end
