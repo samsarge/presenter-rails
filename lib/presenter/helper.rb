9f610a4477fd79beac29ea5d69c696caf85ba3fc
finn-francis <finn_francis@yahoo.com>
2017-12-10 18:17:57 +0000

    Added the ability to pass arguments to #present when calling it on a single object

--- lib/presenter/helper.rb
+++ lib/presenter/helper.rb
@@ -1,12 +1,18 @@
 module Presenter
   module Helper
     include Presenter::Naming
-    def present(object_or_collection)
       if object_or_collection.respond_to?(:map) # If it is a collection
         object_or_collection.map { |object| presenter_from_model_object(object).new(object) }
-      else # If it is a single object
-        presenter_from_model_object(object_or_collection).new(object_or_collection)
+    def present(object_or_collection, *args, &block)
+      else
+        handle_single_object(object_or_collection, *args)
       end
+    end
+
+    def handle_single_object(object, *args)
+      presenter_from_model_object(object).new(object, *args)
+    rescue ArgumentError
+      raise Presenter::Error, Presenter::Error::single_object_argument_error_message
     end
   end
 end
