01ea49e27457467e72ad524af73782e103d08bd6
finn-francis <finn_francis@yahoo.com>
2017-12-10 18:18:23 +0000

    Handle collections for #present

--- lib/presenter/helper.rb
+++ lib/presenter/helper.rb
@@ -1,9 +1,9 @@
 module Presenter
   module Helper
     include Presenter::Naming
-      if object_or_collection.respond_to?(:map) # If it is a collection
-        object_or_collection.map { |object| presenter_from_model_object(object).new(object) }
     def present(object_or_collection, *args, &block)
+      if object_or_collection.respond_to?(:map)
+        handle_collection(object_or_collection, *args, &block)
       else
         handle_single_object(object_or_collection, *args)
       end
@@ -13,6 +13,15 @@ module Presenter
       presenter_from_model_object(object).new(object, *args)
     rescue ArgumentError
       raise Presenter::Error, Presenter::Error::single_object_argument_error_message
+    end
+
+    def handle_collection(collection, *args, &block)
+      collection.map do |object|
+        block_args = block_given? ? block.call(object) : []
+        presenter_from_model_object(object).new(object, *args, *block_args)
+      end
+    rescue ArgumentError
+      raise Presenter::Error, Presenter::Error::collection_argument_error_message
     end
   end
 end
