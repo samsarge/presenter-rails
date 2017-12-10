5e9679502a3a85f93e17b868570e50ed125a921e
finn-francis <finn_francis@yahoo.com>
2017-12-10 18:16:13 +0000

    Added admin model to the dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730114844) do
+ActiveRecord::Schema.define(version: 20171210161614) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
